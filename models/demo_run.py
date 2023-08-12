e27f94a281b277769c75a8dd5f8172dc8c076a52
EngineerDanny <agyapongdaniel7777@gmail.com>
2023-08-11 21:00:01 -0700

    save trained models with hpc

--- models/demo_run.py
+++ models/demo_run.py
@@ -4,11 +4,13 @@ import pandas as pd
 import numpy as np
 import json
 import warnings
-from DataSynthesizer.DataDescriber import DataDescriber
-from DataSynthesizer.DataGenerator import DataGenerator
-from DataSynthesizer.ModelInspector import ModelInspector
-from DataSynthesizer.lib.utils import read_json_file, display_bayesian_network
-import json
+import pickle
+from sklearn.linear_model import *
+from sklearn.model_selection import *
+from sklearn.feature_selection import *
+from sklearn.preprocessing import *
+from sklearn.pipeline import *
+from sklearn.neighbors import *
 
 
 warnings.filterwarnings('ignore')
@@ -21,20 +23,30 @@ else:
     print("len(sys.argv)=%d so trying first param" % len(sys.argv))
     param_row = 0
 
-param_dict = dict(params_df.iloc[param_row, :])
-symbol = param_dict["symbol"]
-
+config_path = "/projects/genomic-ml/da2343/ml_project_2/settings/config.json"
 with open(config_path) as f:
   config = json.load(f)
   
+param_dict = dict(params_df.iloc[param_row, :])
+symbol = param_dict["symbol"]
 model_version = config["model_version"]  
-config_path = "/projects/genomic-ml/da2343/ml_project_2/settings/config.json"
+  
 root_symbol_dir = f"/projects/genomic-ml/da2343/ml_project_2/data/{symbol}"
-root_model_dir = f"/projects/genomic-ml/da2343/ml_project_2/models/{symbol}"
-
-real_data = f"{root_symbol_dir}/{symbol}_H1_2011_2015_TRADES_REAL.csv"
+root_model_dir = f"/projects/genomic-ml/da2343/ml_project_2/models/{model_version}"
 synthetic_data = f"{root_symbol_dir}/{symbol}_H1_2011_2015_TRADES_SYNTHETIC.csv"
 
+trades_df = pd.read_csv(synthetic_data)
+# drop index column
+trades_df = trades_df.drop(columns=['index'])
 
-generator.generate_dataset_in_correlated_attribute_mode(num_tuples_to_generate, description_file)
-generator.save_synthetic_data(synthetic_data)
+X_train, y_train = trades_df.iloc[:, :-1].to_numpy(), trades_df.iloc[:, -1]
+model = LogisticRegressionCV(n_jobs=-1)
+model.fit(X_train, y_train)
+
+# save the model to file
+if not os.path.exists(root_model_dir):
+    os.makedirs(root_model_dir)
+
+
+model_path = f"{root_model_dir}/{symbol.lower()}.pkl"
+pickle.dump(model, open(model_path, "wb"))
