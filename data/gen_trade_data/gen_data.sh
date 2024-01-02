1cc65e551e727277cb96a59b475796a7d86723b1
EngineerDanny <agyapongdaniel7777@gmail.com>
2024-01-01 18:47:49 -0700

    Delete gen_data.sh script for generating trade data

--- data/gen_trade_data/gen_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=gendata         
-#SBATCH --time=24:00:00
-#SBATCH --mem=64GB
-#SBATCH --cpus-per-task=1
-#SBATCH --output=/projects/genomic-ml/da2343/ml_project_2/data/gen_data.out	
-#SBATCH --error=/projects/genomic-ml/da2343/ml_project_2/data/gen_data.err
-
-python /projects/genomic-ml/da2343/ml_project_2/data/gen_trade_data.py
