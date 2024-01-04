e8ce5b92a0d4b33fe91cef7fd966a4acda626d73
EngineerDanny <agyapongdaniel7777@gmail.com>
2024-01-04 14:53:57 -0700

    Update test_ohlcv_data.py with additional data validation tests

--- tests/test_ohlcv_data.py
+++ tests/test_ohlcv_data.py
@@ -6,29 +6,30 @@ data = pd.read_csv("/projects/genomic-ml/da2343/ml_project_2/data/gen_oanda_data
 
 # Define a test class
 class TestData(unittest.TestCase):
-
-    # Define some test methods
-    def test_data_shape(self):
-        # Check that the data has 8 rows and 5 columns
-        self.assertEqual(data.shape, (8, 5))
-
-    def test_data_columns(self):
-        # Check that the data has the expected column names
-        self.assertListEqual(list(data.columns), ["Open", "High", "Low", "Close", "Volume"])
-
     def test_data_values(self):
-        # Check that the data values are positive
-        self.assertTrue((data > 0).all().all())
+        # Check that the data values are not None
+        self.assertTrue((data != None).all().all())
+        # Check the the Open, High, Low, Close and Volume columns values are greater than 0
+        self.assertTrue((data["Open"] > 0).all())
+        self.assertTrue((data["High"] > 0).all())
+        self.assertTrue((data["Low"] > 0).all())
+        self.assertTrue((data["Close"] > 0).all())
+        self.assertTrue((data["Volume"] > 0).all())
 
     def test_data_consistency(self):
-        # Check that the open, high, low and close values are equal for each row
-        self.assertTrue((data["Open"] == data["High"]).all())
-        self.assertTrue((data["Open"] == data["Low"]).all())
-        self.assertTrue((data["Open"] == data["Close"]).all())
+        # Check that the open, high, low and close values are not equal for each row
+        self.assertTrue((data["Open"] != data["High"]).all())
+        self.assertTrue((data["Open"] != data["Low"]).all())
+        self.assertTrue((data["Open"] != data["Close"]).all())
 
     def test_data_volume(self):
-        # Check that the volume is 2 for each row
         self.assertTrue((data["Volume"] > 0).all())
+        
+    def test_data_time(self):
+        # Check that the time is increasing
+        # the time column is data["time"]
+        self.assertTrue((data["time"].diff() > pd.Timedelta(0)).all())
+        
 
 # Run the tests
 if __name__ == "__main__":
