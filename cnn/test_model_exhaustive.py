86e2207325f277f6a4953d2c2e6689f95444bec7
EngineerDanny <agyapongdaniel7777@gmail.com>
2024-01-23 22:59:22 -0700

    Remove unused imports and variables in test_model_exhaustive.py

--- cnn/test_model_exhaustive.py
+++ cnn/test_model_exhaustive.py
@@ -7,25 +7,16 @@ import talib
 import json
 import warnings
 import matplotlib.pyplot as plt
-from sklearn.metrics import roc_curve, auc
-from PIL import Image
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import json
 import time
 import datetime as dt
 import requests
-import sys
 import re
 import itertools
-import os
 import talib
 import logging
 from decimal import Decimal
 import io
-import sys
-import warnings
 
 warnings.filterwarnings("ignore")
 params_df = pd.read_csv("params.csv")
@@ -39,24 +30,16 @@ else:
 
 param_dict = dict(params_df.iloc[param_row, :])
 dataset_name = param_dict["dataset_name"]
-threshold = param_dict["threshold"]
+strategy = param_dict["strategy"]
+atr_delta = param_dict["atr_delta"]
 
 # Load the config file
 config_path = "/projects/genomic-ml/da2343/ml_project_2/settings/config.json"
 with open(config_path) as f:
     config = json.load(f)
 
-# Get the take_profit and stop_loss levels from the config file
-rr_ratio = config["risk_reward_ratio"]
 config_settings = config["trading_settings"][dataset_name]
-sl = config_settings["stop_loss"]
-tp = rr_ratio * sl
 start_hr = config_settings["start_hour"]
 end_hr = config_settings["end_hour"]
-window_size = config["window_size"]
 
-# dataset_path = config_settings["dataset_path"]
 root_data_dir = config["paths"]["oanda_dir"]
-device = config["device"]
-# ml_model_path = config["paths"]["model_80_dir"]
-ml_model_path = config["paths"]["model_90_dir"]
