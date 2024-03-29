f607c06b2d659fa7827219bda407f2ce1e62c1f5
EngineerDanny <agyapongdaniel7777@gmail.com>
2024-03-29 10:18:26 -0700

    Add new time value in analyze.py

--- kmeans/analyze.py
+++ kmeans/analyze.py
@@ -28,6 +28,7 @@ time = "2024-03-23_07:49"
 time = "2024-03-25_01:47"
 
 time = "2024-03-29_04:53"
+time = "2024-03-29_07:23"
 
 out_df_list = []
 for out_csv in glob(f"/scratch/da2343/ml_project_2_{time}/results/*.csv"):
