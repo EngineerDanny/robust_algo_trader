b1963a5dd5e6a685e30cfbc8f52304a952c1bf54
EngineerDanny <agyapongdaniel7777@gmail.com>
2024-12-12 15:58:58 -0700

    Add parameter generation and job script creation for k-means clustering

--- unsupervised/kmeans/analyze.py
+++ unsupervised/kmeans/analyze.py
@@ -1,27 +1,10 @@
 import pandas as pd
 from glob import glob
 
-time = "2024-08-15_21:03" # EUR_USD_M15
-time = "2024-08-15_21:08" # GBP_USD_M15
-time = "2024-08-15_21:11" # USD_JPY_M15
-time = "2024-08-15_21:13" # USD_CHF_M15
-time = "2024-08-15_21:15" # AUD_JPY_M15
-time = "2024-08-15_21:18" # USD_CAD_M15
-time = "2024-08-15_21:19" # AUD_USD_M15
-time = "2024-08-15_21:22" # AUD_CAD_M15
-
-time = "2024-08-15_21:23" # EUR_GBP_M15 
-time = "2024-08-15_21:25" # EUR_JPY_M15
-time = "2024-08-15_21:27" # GBP_CHF_M15
-time = "2024-08-15_21:31" # GBP_JPY_M15
-time = "2024-08-15_21:33" # EUR_CHF_M15
-time = "2024-08-15_21:34" # AUD_NZD_M15
-time = "2024-08-15_21:36" # CAD_JPY_M15
-time = "2024-08-15_21:38" # NZD_USD_M15
-time = "2024-08-15_21:55" # EUR_CAD_M15
-
 time = "2024-09-27_03:27" # EUR_USD_M15 - AUD_CAD_M15
 time = "2024-09-27_03:35" # EUR_GBP_M15 - EUR_CAD_M15
+time = "2024-12-12_12:01"
+time = "2024-12-12_14:03"
 
 out_df_list = []
 for out_csv in glob(f"/scratch/da2343/ml_project_2_{time}/results/*.csv"):
---
+++ unsupervised/kmeans/params_gfd.py
@@ -0,0 +1,91 @@
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import os
+import shutil
+import sys
+import json
+
+params_df_list = []
+params_dict = {
+    # "instrument": ["GBP_USD_M15", "USD_JPY_M15"],
+    # "instrument": ["EUR_USD_M15", "GBP_USD_M15", "USD_JPY_M15",
+    #                "USD_CHF_M15", "USD_CAD_M15", "AUD_USD_M15",
+    #                "AUD_JPY_M15", "AUD_CAD_M15"
+    #                ],
+    "instrument": [
+        "EUR_USD_M15",
+        "GBP_USD_M15",
+        "USD_JPY_M15",
+        "USD_CHF_M15",
+        "USD_CAD_M15",
+        "AUD_USD_M15",
+        "AUD_JPY_M15",
+        "AUD_CAD_M15",
+        "EUR_GBP_M15",
+        "EUR_JPY_M15",
+        "GBP_CHF_M15",
+        "GBP_JPY_M15",
+        "EUR_CHF_M15",
+        "AUD_NZD_M15",
+        "CAD_JPY_M15",
+        "NZD_USD_M15",
+        "EUR_CAD_M15",
+    ],
+    # "price_history_length": [24, 36],
+    "price_history_length": [24],
+    "num_perceptually_important_points": [4],
+    "num_clusters": [4, 5, 6, 7, 8, 9, 10],
+    # "clustering_algorithm": ["kmeans", "birch", "gaussian_mixture"],
+    "clustering_algorithm": ["kmeans", "birch"],
+    "random_seed": np.random.choice(1000, size=2, replace=False),
+    "train_period": [4, 5, 6, 7, 8, 9, 10, 11, 12, 14],  # weeks
+    "test_period": [1],  # weeks
+    "reverse_test": [True, False],
+}
+params_df = (
+    pd.MultiIndex.from_product(params_dict.values(), names=params_dict.keys())
+    .to_frame()
+    .reset_index(drop=True)
+)
+params_df_list.append(params_df)
+params_concat_df = pd.concat(params_df_list, ignore_index=True)
+
+n_tasks, ncol = params_concat_df.shape
+date_time = datetime.now().strftime("%Y-%m-%d_%H:%M")
+job_name = f"ml_project_2_{date_time}"
+job_dir = "/scratch/da2343/" + job_name
+results_dir = os.path.join(job_dir, "results")
+os.system("mkdir -p " + results_dir)
+params_concat_df.to_csv(os.path.join(job_dir, "params.csv"), index=False)
+
+print(f"created {n_tasks} tasks in {job_dir}")
+
+run_one_contents = f"""#!/bin/bash
+#SBATCH --array=0-{n_tasks-1}
+#SBATCH --time=24:00:00
+#SBATCH --mem=4GB
+#SBATCH --cpus-per-task=1
+#SBATCH --error={job_dir}/slurm-%A_%a.out
+#SBATCH --output={job_dir}/slurm-%A_%a.out
+#SBATCH --job-name={job_name}
+cd {job_dir}
+python run_one.py $SLURM_ARRAY_TASK_ID
+"""
+run_one_sh = os.path.join(job_dir, "run_one.sh")
+with open(run_one_sh, "w") as run_one_f:
+    run_one_f.write(run_one_contents)
+
+run_orig_py = "demo_run_gfd.py"
+run_one_py = os.path.join(job_dir, "run_one.py")
+shutil.copyfile(run_orig_py, run_one_py)
+orig_dir = os.path.dirname(run_orig_py)
+orig_results = os.path.join(orig_dir, "results")
+os.system("mkdir -p " + orig_results)
+orig_csv = os.path.join(orig_dir, "params.csv")
+params_concat_df.to_csv(orig_csv, index=False)
+
+msg = f"""created params CSV files and job scripts, test with
+python {run_orig_py}
+SLURM_ARRAY_TASK_ID=0 bash {run_one_sh}"""
+print(msg)
